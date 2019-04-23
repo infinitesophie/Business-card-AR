7e2e86d8bbcfd555da87abe8352a3f1658cb2dbe
sam <sam685856@gmail.com>
2019-04-23 08:46:03 +0200

    importing project
    initial commit

---
+++ Assets/Script/OnClickForGirl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnClickForGirl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Update()
+    {
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Transform select = GameObject.FindWithTag("select").transform;
+            if (Physics.Raycast(Vector2(ray), hit, 100))
+            {
+                select.tag = "none";
+                hit.collider.transform.tag = "select";
+            }
+        }
+    }
+}
+
---
+++ Assets/Script/SocialMedia.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SocialMedia : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void OpenFacebook()
+    {
+
+        Application.OpenURL("https://www.facebook.com/sophie.me.0");
+    }
+    public void OpenLinkdIn()
+    {
+        Application.OpenURL("https://www.linkedin.com/in/sophiamary/");
+    }
+}
